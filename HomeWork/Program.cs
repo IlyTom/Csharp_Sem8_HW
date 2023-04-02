675b1464744e3effc268f84f42ccc848f0c3acbb
IlyTom <126102373+IlyTom@users.noreply.github.com>
2023-04-02 15:54:36 +0300

    4 задача

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,76 +1,80 @@
-﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-// Например, даны 2 матрицы:
-// 2 4 | 3 4
-// 3 2 | 3 3
-// Результирующая матрица будет:
-// 18 20
-// 15 18
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+int rand()
+{
+    int a = new Random().Next(10, 100);
+    return a;
+}
 
-int[,] FillArray()
+int[,,] FillArray()
 {
     System.Console.Write("Введите кол-во строк массива: ");
     int m = int.Parse(Console.ReadLine()!);
     System.Console.Write("Введите кол-во столбцов массива: ");
     int n = int.Parse(Console.ReadLine()!);
-    int[,] Array = new int[m, n];
+    System.Console.Write("Введите глубину массива: ");
+    int l = int.Parse(Console.ReadLine()!);
+    int[] arr = new int[m * n * l];
+    for (int i = 0; i < m * n * l; i++)
+    {
+        if (m*n*l<=90) break;
+        int num;
+        arr[i] = rand();
+        num = arr[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (arr[i] == arr[j])
+                {
+                    arr[i] = rand();
+                    j = 0;
+                    num = arr[i];
+                }
+                num = arr[i];
+            }
+        }
+    }
+    int el = 0;
+    int[,,] Array = new int[m, n, l];
     for (int i = 0; i < Array.GetLength(0); i++)
+    {
         for (int j = 0; j < Array.GetLength(1); j++)
         {
-            Array[i, j] = new Random().Next(1, 10);
+            for (int k = 0; k < Array.GetLength(2); k++)
+            {
+                Array[i, j, k] = arr[el];
+                el++;
+            }
         }
+    }
     return Array;
 }
 
-void PrintArray(int[,] Array)
+
+
+
+void PrintArray(int[,,] Array)
 {
     for (int i = 0; i < Array.GetLength(0); i++)
     {
         for (int j = 0; j < Array.GetLength(1); j++)
         {
-            Console.Write($"{Array[i, j]} ");
-        }
-        System.Console.WriteLine();
-    }
-}
-
-
-void MultiplyArrays(int[,] arr1, int[,] arr2)
-{
-    while (true)
-    {
-        if (arr1.GetLength(0) == arr2.GetLength(1))
-        {
-            int[,] arr3 = new int[arr1.GetLength(0), arr2.GetLength(1)];
-            for (int i = 0; i < arr1.GetLength(0); i++)
+            for (int k = 0; k < Array.GetLength(2); k++)
             {
-                for (int j = 0; j < arr2.GetLength(1); j++)
-                {
-                    arr3[i, j] = 0;
-                    for (int k = 0; k < arr1.GetLength(1); k++)
-                    {
-                        arr3[i, j] += arr1[i, k] * arr2[k, j];
-                    }
-                }
+                Console.Write($"{Array[i, j, k]} ({i},{j},{k}) ");
             }
             System.Console.WriteLine();
-            PrintArray(arr3);
-            break;
-        }
-        else
-        {
-            System.Console.WriteLine("Введены неверные размеры матриц!!!");
-            break;
         }
     }
 }
 
-int[,] arr1 = FillArray();
-PrintArray(arr1);
-System.Console.WriteLine();
-int[,] arr2 = FillArray();
-PrintArray(arr2);
-System.Console.WriteLine();
-MultiplyArrays(arr1, arr2);
+int[,,] Array3 = FillArray();
+PrintArray(Array3);
 
 
 
